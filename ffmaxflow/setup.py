c7cfd8cf5f7c64d82f9c9fbedd565a0c744806b1
diramtz <nancy.dira@gmail.com>
2021-03-27 13:38:50 -0600

    adding long description to package

--- ffmaxflow/setup.py
+++ ffmaxflow/setup.py
@@ -10,10 +10,10 @@ classifiers = [
  
 setup(
   name='ffmaxflow',
-  version='0.0.3',
+  version='0.0.4',
   description='Implementation of the  Fork Fulkerson method to find the max posible flow of a network',
-  #long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
-  url='',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='https://optimizacion-2-2021-1-gh-classroom.github.io/practica-1-segunda-parte-diramtz/maxflow.html',
   author='Equipo 2',
   author_email='a@b.c',
   license='MIT', 
